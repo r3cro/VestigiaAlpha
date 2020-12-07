6b1ef3b8676bcf197b4e2d187ac6eea6a9ed4b09
r3cro <lmanlief1@ivytech.edu>
2020-12-07 14:06:44 -0500

    init

---
+++ cmake-build-debug/CMakeFiles/VestigiaAlpha.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VestigiaAlpha.dir/main.cpp.obj"
+  "VestigiaAlpha.exe"
+  "VestigiaAlpha.exe.manifest"
+  "VestigiaAlpha.pdb"
+  "libVestigiaAlpha.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VestigiaAlpha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
